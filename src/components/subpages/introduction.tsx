7a407d062034a1f7d191a453333d82d8f928e3b9
cpustejovsky <charles.pustejovsky@gmail.com>
2022-10-21 20:54:50 -0400

    feat: Update introduction copy and resume

--- src/components/subpages/introduction.tsx
+++ src/components/subpages/introduction.tsx
@@ -1,28 +1,29 @@
+// @ts-ignore
 import React from "react"
-import { Grid } from "@material-ui/core"
+import {Grid} from "@material-ui/core"
 import Cpustejovsky from "../images/cpustejovsky"
 
 const Introduction = () => (
     <Grid container className="introduction">
-      <Grid item xs={12} sm={5}>
-        <Cpustejovsky/>
-      </Grid>
-      <Grid item className="padding-top" xs={12} sm={7}>
-        <h3>Howdy!</h3>
-        <p>I'm Charles Pustejovsky.</p>
-        <p>
-          Some things you're good at. Some things are good for you.
-        </p>
-        <p>
-          For me, programming is both. That's why I love it.
-        </p>
-        <p>
-          It's why I love Go, TDD, and SDLC methodologies.
-        </p>
-        <p>
-          They all can help me to better provide value to my team and my organization.
-        </p>
-      </Grid>
+        <Grid item xs={12} sm={5}>
+            <Cpustejovsky/>
+        </Grid>
+        <Grid item className="padding-top" xs={12} sm={7}>
+            <h3>Howdy!</h3>
+            <p>
+                I'm Charles Pustejovsky.
+            </p>
+            <p>
+                I'm a software engineer who is passionate about providing value with programming.
+            </p>
+            <p>
+                I use Go, Test-Driven Development
+                and SDLC methodologies to do this.
+            </p>
+            <p>
+                I focus on empathy, continuing education, and skills improvement.
+            </p>
+        </Grid>
     </Grid>
 )
 
